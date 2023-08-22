0b74665d91068a9a3f1aa9ac91f9075e45cc8a46
Theo Sanderson <theo@sndrsn.co.uk>
2023-08-22 16:30:20 +0100

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai'
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
-  apiBase: "https://openrouter.ai/api/v1"
+  baseURL: "https://openrouter.ai/api/v1"
 })
 
 // IMPORTANT! Set the runtime to edge
